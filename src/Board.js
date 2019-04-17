d223de060a27ccac73217f16ec32c4923537d231
dustintktran <dustintktran@gmail.com>
2019-04-17 16:51:13 -0700

    Fully implemented Board helper functions

--- src/Board.js
+++ src/Board.js
@@ -79,13 +79,17 @@
     //
     // test if a specific row on this board contains a conflict
     hasRowConflictAt: function(rowIndex) {
+      var number = 0;
 
       for (var i = 0; i < this.attributes[rowIndex].length; i++) {
         if (this.attributes[rowIndex][i] > 0) {
-          return true;
+          number++;
         };
       };
-      
+
+      if(number > 1) {
+        return true;
+      }
       return false; 
     },
 
@@ -108,24 +112,30 @@
     //
     // test if a specific column on this board contains a conflict
     hasColConflictAt: function(colIndex) {
+      var number = 0;
 
       for (var i = 0; i < this.attributes.n; i++) {
         if (this.attributes[i][colIndex] > 0){
-          return true;
-        }
-      }
+          number++;
+        };
+      };
 
-      return false; // fixme
+      if (number > 1) {
+        return true;
+      };
+
+      return false;
     },
 
     // test if any columns on this board contain conflicts
     hasAnyColConflicts: function() {
-      for(var i = 0; i < this.attributes.n; i++){
-        if(this.hasColConflictAt(i) === true){
+      for (var i = 0; i < this.attributes.n; i++) {
+        if (this.hasColConflictAt(i) === true) {
           return true;
-        }
-      }
-      return false; // fixme
+        };
+      };
+
+      return false;
     },
 
 
@@ -135,12 +145,33 @@
     //
     // test if a specific major diagonal on this board contains a conflict
     hasMajorDiagonalConflictAt: function(majorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var number = 0;
+      
+      for (var i = 0; i < this.attributes.n; i++) {
+        for (var j = 0; j < this.attributes.n; j++) {
+          if (majorDiagonalColumnIndexAtFirstRow === i-j && this.attributes[i][j] == 1) {
+            number++;
+          };
+        };
+      };
+      
+      if (number > 1) {
+        return true;
+      };
+
+      return false;
     },
 
     // test if any major diagonals on this board contain conflicts
     hasAnyMajorDiagonalConflicts: function() {
-      return false; // fixme
+      var count = 0 - this.attributes.n;
+      for (var i = count; i < this.attributes.n; i++) {
+        if (this.hasMajorDiagonalConflictAt(i) === true) {
+          return true;
+        };
+      };
+
+      return false;
     },
 
 
@@ -150,25 +181,53 @@
     //
     // test if a specific minor diagonal on this board contains a conflict
     hasMinorDiagonalConflictAt: function(minorDiagonalColumnIndexAtFirstRow) {
-      return false; // fixme
+      var number = 0;
+      
+      for (var i = 0; i < this.attributes.n; i++) {
+        for (var j = 0; j < this.attributes.n; j++) {
+          if (minorDiagonalColumnIndexAtFirstRow === i+j && this.attributes[i][j] == 1) {
+            number++;
+          };
+        };
+      };
+      
+      if (number > 1) {
+        return true;
+      };
+      
+      return false;
     },
 
     // test if any minor diagonals on this board contain conflicts
     hasAnyMinorDiagonalConflicts: function() {
-      return false; // fixme
+      var count = 2 * this.attributes.n;
+      for (var i = count; i > 0; i--) {
+        if (this.hasMinorDiagonalConflictAt(i) === true) {
+          return true;
+        };
+      };
+      
+      return false;
     }
 
     /*--------------------  End of Helper Functions  ---------------------*/
 
-
   });
 
-  var makeEmptyMatrix = function(n) {
-    return _(_.range(n)).map(function() {
-      return _(_.range(n)).map(function() {
-        return 0;
-      });
-    });
-  };
+  // var makeEmptyMatrix = function(n) {
+  //   return _(_.range(n)).map(function() {
+  //     return _(_.range(n)).map(function() {
+  //       return 0;
+  //     });
+  //   });
+  // };
 
 }());
+
+var makeEmptyMatrix = function(n) {
+  return _(_.range(n)).map(function() {
+    return _(_.range(n)).map(function() {
+      return 0;
+    });
+  });
+};
